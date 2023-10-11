364db269dc8f1c58545adbb7f3971d594efadc92
Hemant1803 <hshemantsharma445@gmail.com>
2023-10-11 22:06:56 +0530

    Count Of Remaining Chapter

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Read_chapter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+                int N = 4; 
+                Dictionary<int, int> chapterPages = new Dictionary<int, int>
+        {
+            { 1, 4 },
+            { 5, 7 },
+            { 9, 16 },
+            { 17, 26 }
+        };
+                int K = 7; 
+                int chaptersYetToRead = 0;
+                foreach (var chapter in chapterPages)
+                {
+                    if (chapter.Key <= K && K <= chapter.Value)
+                    {
+                        
+                        break;
+                    }
+                    else
+                    {
+                      
+                        chaptersYetToRead++;
+                    }
+                }
+
+             
+                chaptersYetToRead++;
+
+               
+                Console.WriteLine("Number of chapters to read: " + chaptersYetToRead);
+            
+        }
+    }
+}
